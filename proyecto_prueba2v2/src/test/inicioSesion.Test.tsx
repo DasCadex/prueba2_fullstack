6b63e8eafcd5eb2dd08e6c9df48af4f5b9019d8b
ma-samaniego <ma.samaniego@duocuc.cl>
2025-10-28 15:29:29 -0300

    cambio de nombre test

--- proyecto_prueba2v2/src/test/inicioSesion.Test.tsx
+++ proyecto_prueba2v2/src/test/inicioSesion.Test.tsx
@@ -1,3 +1,4 @@
+//import
 import { describe, it, expect, vi } from "vitest";
 import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
